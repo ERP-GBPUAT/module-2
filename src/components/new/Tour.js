96b290251b9af20b13697406847a19c494b66ba6
Udit <royalboy0012@gmail.com>
2023-06-20 02:52:27 +0530

    validation added

--- src/components/new/Tour.js
+++ src/components/new/Tour.js
@@ -1,14 +1,72 @@
-import { useContext } from 'react';
-import FormContext from 'context';
-import { useNavigate } from 'react-router-dom';
-import { Form, Col, Container, Row, Button } from 'react-bootstrap';
+import { useContext } from "react";
+import FormContext from "context";
+import { useNavigate } from "react-router-dom";
+import { Form, Col, Container, Row, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const Tour = () => {
   const { data, setData } = useContext(FormContext);
+  const {
+    register,
+    handleSubmit,
+    control,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      tour1: "",
+      year1: "",
+      lf1: "",
+      amount1: "",
+      tour2: "",
+      year2: "",
+      lf2: "",
+      amount2: "",
+      tour3: "",
+      year3: "",
+      lf3: "",
+      amount3: "",
+    },
+  });
   const navigate = useNavigate();
-  const handleFormSubmit = () => {
-    console.log(data);
-    navigate('/food');
+  const handleFormSubmit = async(formData) => {
+    console.log("formData",formData);
+    console.log("fill Data",data);
+    try {
+      const res = await fetch("http://localhost:8080/nodues/applyNodue", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        "token": localStorage.getItem("token"),
+      },
+      body: JSON.stringify({
+    accountName: data.accountName,
+    accountNumber: data.accountNumber,
+    bankName: data.bankName,
+    bankBranch: data.bankBranch,
+    
+    tour1: formData.tour1,
+    year1: formData.year1,
+    lf1: formData.lf1,
+    amount1: formData.amount1,
+    tour2: formData.tour2,
+    year2: formData.year2,
+    lf2: formData.lf2,
+    amount2: formData.amount2,
+    tour3: formData.tour3,
+    year3: formData.year3,
+    lf3: formData.lf3,
+    amount3: formData.amount3,
+      }),
+    });
+    const json =await res.json();
+    console.log(json);
+    if(json.msg==="success"){
+      navigate("/")
+    }
+  } catch (error) {
+    console.log(error);
+  }
   };
   const handleChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
@@ -16,7 +74,7 @@ const Tour = () => {
   return (
     <Container className="d-flex flex-column">
       <h3 className="text-center mt-4 text-bold">Enter Tour Details</h3>
-      <Form onSubmit={handleFormSubmit}>
+      <Form onSubmit={handleSubmit(handleFormSubmit)}>
         <Row className="justify-content-center mt-4">
           <Col xs={12} md={3}>
             <Form.Group controlId="tour1">
@@ -24,10 +82,25 @@ const Tour = () => {
               <Form.Control
                 type="text"
                 name="tour1"
-                value={data.tour1}
-                onChange={handleChange}
-                required
+                {...register("tour1", {
+                  required: {
+                    value: true,
+                    message: "This field is required",
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/i,
+                    message: "Enter a valid tour name",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "Tour name length should be atleast 3",
+                  },
+                })}
+                isInvalid={errors.tour1}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors?.tour1?.type ? <p>{errors?.tour1?.message}</p> : ""}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group controlId="year1">
@@ -35,10 +108,29 @@ const Tour = () => {
               <Form.Control
                 type="text"
                 name="year1"
-                value={data.year1}
-                onChange={handleChange}
-                required
+                {...register("year1", {
+                  required: {
+                    value: true,
+                    message: "This field is required",
+                  },
+                  pattern: {
+                    value: /^[0-9+-]+$/,
+                    message: "Enter a valid tour name",
+                  },
+                  minLength: {
+                    value: 4,
+                    message: "Enter a valid Year",
+                  },
+                  maxLength:{
+                    value:4,
+                    message:"Enter a valid year"
+                  }
+                })}
+                isInvalid={errors.year1}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors?.year1?.type ? <p>{errors?.year1?.message}</p> : ""}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group controlId="lf1">
@@ -46,10 +138,21 @@ const Tour = () => {
               <Form.Control
                 type="text"
                 name="lf1"
-                value={data.lf1}
-                onChange={handleChange}
-                required
+                {...register("lf1", {
+                  required: {
+                    value: true,
+                    message: "This field is required",
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/i,
+                    message: "Enter a valid lf no",
+                  },
+                })}
+                isInvalid={errors.lf1}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors?.lf1?.type ? <p>{errors?.lf1?.message}</p> : ""}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group controlId="amount1">
@@ -57,10 +160,21 @@ const Tour = () => {
               <Form.Control
                 type="text"
                 name="amount1"
-                value={data.amount1}
-                onChange={handleChange}
-                required
+                {...register("amount1", {
+                  required: {
+                    value: true,
+                    message: "This field is required",
+                  },
+                  pattern: {
+                    value: /^[0-9+-]+$/,
+                    message: "Enter a valid amount",
+                  },
+                })}
+                isInvalid={errors.amount1}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors?.amount1?.type ? <p>{errors?.amount1?.message}</p> : ""}
+              </Form.Control.Feedback>
             </Form.Group>
           </Col>
           <Col xs={12} md={3}>
@@ -69,10 +183,25 @@ const Tour = () => {
               <Form.Control
                 type="text"
                 name="tour2"
-                value={data.tour2}
-                onChange={handleChange}
-                required
+                {...register("tour2", {
+                  required: {
+                    value: true,
+                    message: "This field is required",
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/i,
+                    message: "Enter a valid tour name",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "Tour name length should be atleast 3",
+                  },
+                })}
+                isInvalid={errors.tour2}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors?.tour2?.type ? <p>{errors?.tour2?.message}</p> : ""}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group controlId="year2">
@@ -80,10 +209,29 @@ const Tour = () => {
               <Form.Control
                 type="text"
                 name="year2"
-                value={data.year2}
-                onChange={handleChange}
-                required
+                {...register("year2", {
+                  required: {
+                    value: true,
+                    message: "This field is required",
+                  },
+                  pattern: {
+                    value: /^[0-9+-]+$/,
+                    message: "Enter a valid tour name",
+                  },
+                  minLength: {
+                    value: 4,
+                    message: "Enter a valid Year",
+                  },
+                  maxLength:{
+                    value:4,
+                    message:"Enter a valid year"
+                  }
+                })}
+                isInvalid={errors.year2}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors?.year2?.type ? <p>{errors?.year2?.message}</p> : ""}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group controlId="lf2">
@@ -91,10 +239,21 @@ const Tour = () => {
               <Form.Control
                 type="text"
                 name="lf2"
-                value={data.lf2}
-                onChange={handleChange}
-                required
+                {...register("lf2", {
+                  required: {
+                    value: true,
+                    message: "This field is required",
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/i,
+                    message: "Enter a valid lf no",
+                  },
+                })}
+                isInvalid={errors.lf2}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors?.lf2?.type ? <p>{errors?.lf2?.message}</p> : ""}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group controlId="amount2">
@@ -102,10 +261,21 @@ const Tour = () => {
               <Form.Control
                 type="text"
                 name="amount2"
-                value={data.amount2}
-                onChange={handleChange}
-                required
+                {...register("amount2", {
+                  required: {
+                    value: true,
+                    message: "This field is required",
+                  },
+                  pattern: {
+                    value: /^[0-9+-]+$/,
+                    message: "Enter a valid amount",
+                  },
+                })}
+                isInvalid={errors.amount2}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors?.amount2?.type ? <p>{errors?.amount2?.message}</p> : ""}
+              </Form.Control.Feedback>
             </Form.Group>
           </Col>
           <Col xs={12} md={3}>
@@ -114,10 +284,25 @@ const Tour = () => {
               <Form.Control
                 type="text"
                 name="tour3"
-                value={data.tour3}
-                onChange={handleChange}
-                required
+                {...register("tour3", {
+                  required: {
+                    value: true,
+                    message: "This field is required",
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/i,
+                    message: "Enter a valid tour name",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "Tour name length should be atleast 3",
+                  },
+                })}
+                isInvalid={errors.tour3}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors?.tour3?.type ? <p>{errors?.tour3?.message}</p> : ""}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group controlId="year3">
@@ -125,10 +310,29 @@ const Tour = () => {
               <Form.Control
                 type="text"
                 name="year3"
-                value={data.year3}
-                onChange={handleChange}
-                required
+                {...register("year3", {
+                  required: {
+                    value: true,
+                    message: "This field is required",
+                  },
+                  pattern: {
+                    value: /^[0-9+-]+$/,
+                    message: "Enter a valid tour name",
+                  },
+                  minLength: {
+                    value: 4,
+                    message: "Enter a valid Year",
+                  },
+                  maxLength:{
+                    value:4,
+                    message:"Enter a valid year"
+                  }
+                })}
+                isInvalid={errors.year3}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors?.year3?.type ? <p>{errors?.year3?.message}</p> : ""}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group controlId="lf3">
@@ -136,20 +340,42 @@ const Tour = () => {
               <Form.Control
                 type="text"
                 name="lf3"
-                value={data.lf3}
-                onChange={handleChange}
-                required
+                {...register("lf3", {
+                  required: {
+                    value: true,
+                    message: "This field is required",
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/i,
+                    message: "Enter a valid lf no",
+                  },
+                })}
+                isInvalid={errors.lf3}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors?.lf3?.type ? <p>{errors?.lf3?.message}</p> : ""}
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId="amount3">
               <Form.Label>Amount Due</Form.Label>
               <Form.Control
                 type="text"
                 name="amount3"
-                value={data.amount3}
-                onChange={handleChange}
-                required
+                {...register("amount3", {
+                  required: {
+                    value: true,
+                    message: "This field is required",
+                  },
+                  pattern: {
+                    value: /^[0-9+-]+$/,
+                    message: "Enter a valid amount",
+                  },
+                })}
+                isInvalid={errors.amount3}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors?.amount3?.type ? <p>{errors?.amount3?.message}</p> : ""}
+              </Form.Control.Feedback>
             </Form.Group>
           </Col>
           <div className="d-flex justify-content-between gap-20">
@@ -157,13 +383,13 @@ const Tour = () => {
               variant="danger"
               type="button"
               onClick={() => {
-                navigate('/caution');
+                navigate("/caution");
               }}
             >
               Prev
             </Button>
             <Button variant="primary" type="submit">
-              Next
+              Submit
             </Button>
           </div>
         </Row>
